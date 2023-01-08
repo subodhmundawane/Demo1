1e52618849660bd83b2bfd6c6e0ea19e26b0a730
subodhmundawane <subodhmundawane123@gmail.com>
2023-01-08 15:39:22 +0530

    sujit first save

---
+++ src/com/velocity/Addition.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+public class Addition {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 5;
+		int c ;
+		 
+		c= a+b;
+		System.out.println("Addition of two number"+c);
+		
+	}
+
+}
